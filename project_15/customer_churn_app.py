9007c656b99632f3d10d5e9cdec40180821b9f01
Chan Chong Wee <142921480+chongwee0312@users.noreply.github.com>
2024-03-20 21:21:06 +0800

    Update customer_churn_app.py

--- project_15/customer_churn_app.py
+++ project_15/customer_churn_app.py
@@ -169,8 +169,7 @@ if data and ('churn' in df_pred.columns) and ('customer_id' not in df.columns):
         with col1:            
             plt.figure(figsize=(3, 2))
             sns.barplot(y=result['churn'])
-            # plt.pie(result['count'], labels=result.index, autopct='%1.2f%%', startangle=90,
-            #         shadow=True, explode=(0, 0.1), colors=['tomato', 'steelblue'], textprops={'fontsize': 8})
+            sns.barplot(x=result.index, y=result['count'], hue=result.index, palette=['steelblue', 'tomato'])
             plt.title('Customer Churn Rate', y=1.0, fontsize=7, fontweight='bold')
             plt.show()
             st.pyplot(plt)
