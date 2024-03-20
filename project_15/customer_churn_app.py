4e0aaa7c42720e0f429c378ac5a876616c0fb52a
Chan Chong Wee <142921480+chongwee0312@users.noreply.github.com>
2024-03-20 21:03:08 +0800

    Update customer_churn_app.py

--- project_15/customer_churn_app.py
+++ project_15/customer_churn_app.py
@@ -155,8 +155,7 @@ if data and ('churn' in df_pred.columns) and ('customer_id' not in df.columns):
     st.header('Prediction Result Analysis')
     
     menu = list(df_pred.columns)
-    for col in ['customer_id', 'begin_date', 'churn']:
-        menu.remove(col)    
+    menu.remove('churn')    
     menu = [' '.join(word.capitalize() for word in x.split('_')) for x in menu]
     menu.insert(0, 'Choose the characteristic')
     menu.insert(1, 'Overall')
