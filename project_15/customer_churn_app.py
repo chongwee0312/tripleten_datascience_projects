de2b13f7f0e32a27b551f8faedd86b7bfbcdf727
Chan Chong Wee <142921480+chongwee0312@users.noreply.github.com>
2024-03-20 20:47:04 +0800

    Update customer_churn_app.py

--- project_15/customer_churn_app.py
+++ project_15/customer_churn_app.py
@@ -174,6 +174,7 @@ if data and ('churn' in df_pred.columns):
             plt.pie(result['count'], labels=result.index, autopct='%1.2f%%', startangle=90,
                     shadow=True, explode=(0, 0.1), colors=['tomato', 'steelblue'], textprops={'fontsize': 8})
             plt.title('Customer Churn Rate', y=1.0, fontsize=7, fontweight='bold')
+            plt.show()
             st.pyplot(plt)
         with col2:
             table = result.copy()
