58aa6b9bf04e9a5320ec74f7bcaf2643baad3957
Chan Chong Wee <142921480+chongwee0312@users.noreply.github.com>
2024-03-20 20:41:58 +0800

    Update customer_churn_app.py

--- project_15/customer_churn_app.py
+++ project_15/customer_churn_app.py
@@ -142,7 +142,7 @@ if data:
 df = st.session_state['df']
 df_pred = st.session_state['df_pred']
 
-if data and ('churn' in df_pred.columns) and (df == df_pred.drop(columns=['churn'])):
+if data and ('churn' in df_pred.columns) and (df['monthly_charges'] == df_pred['monthly_charges'])):
     # Show the prediction
     st.header('Prediction')
     st.write(df_pred)
