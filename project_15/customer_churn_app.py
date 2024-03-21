be6ccb64ec25182cb3a4ce825e44369e92806ee1
Chan Chong Wee <142921480+chongwee0312@users.noreply.github.com>
2024-03-21 22:22:26 +0800

    Update customer_churn_app.py

--- project_15/customer_churn_app.py
+++ project_15/customer_churn_app.py
@@ -182,6 +182,9 @@ if data and ('churn' in df_pred.columns) and ('customer_id' not in df.columns):
             table = result.copy()
             table.rename_axis('', inplace=True)            
             table['percentage'] = round(table['count'] / table['count'].sum() * 100, 2)
+            table['count'] = table['count'].astype(int)  # Convert 'count' column to integers
+            table['percentage'] = table['percentage'].astype(float)
+            
             for _ in range(2):
                 st.write('#')
             st.table(table.style.format("{:.2f}"))
